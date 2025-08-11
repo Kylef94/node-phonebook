23dbf56f3e119649b380cac140b21be5fa2f6b75
kyle farrugia <k.farrugia94@gmail.com>
2025-08-11 16:24:44 +0100

    cleaned up repo as per exercise instructions

--- frontend/src/components/ContactList.jsx
+++
@@ -1,9 +0,0 @@
-const Person = ({person, removeContact}) => <p>{person.name} {person.number} <button onClick={() => removeContact(person)}>Delete</button></p>
-
-const ContactList = ({persons, search, removeContact}) => {
-  const filterByName = () => persons.filter((person) => person.name.toLowerCase().includes(search.toLowerCase()))
-  
-  return (filterByName().map((person) => <Person key={person.name} person={person} removeContact={removeContact} />))
-}
-
-export default ContactList
